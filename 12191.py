0e9cfeb17846ffc7ce2006f3b193514883e865dc
DevikShah07 <107706364+DevikShah07@users.noreply.github.com>
2023-11-21 10:22:53 +0530

    Update 12191.py

--- 12191.py
+++ 12191.py
@@ -1,44 +1,23 @@
-import random
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans 
+from sklearn.datasets import make_blobs 
 
-def dist(point1, point2):
-    # calculating distance between 2 points
-    return sum((x-y) ** 2 for x, y in zip(point1, point2)) ** 0.5
+n_samples = 300 
+n_features = 2 
+n_clusters = 3 
+X, y = make_blobs(n_samples=n_samples, n_features=n_features, centers=n_clusters, random_state=42) 
+ 
+kmeans = KMeans(n_clusters=n_clusters) 
+kmeans.fit(X) 
+y_kmeans = kmeans.predict(X) 
+centers = kmeans.cluster_centers_ 
 
-def cluster_points(points, centroids):
-    # assigning cluster points
-    clusters = [[] for i in range(len(centroids))]
-    
-    for point in points:
-        distances = [dist(point, centroid) for centroid in centroids]
-        closest_cluster_index = distances.index(min(distances))
-        clusters[closest_cluster_index].append(point)
-    return clusters
-
-def update_centroids(clusters):
-    # to update centroids
-    return [tuple(sum(x) / len(x) for x in zip(*cluster)) for cluster in clusters]
-
-def k_means(points, k, max_iterations = 100):
-    centroids = random.sample(points, k)  # randomly assiging 3 centroids
-    
-    for i in range(max_iterations):
-        clusters = cluster_points(points, centroids)  # nearest centroid data point assingning
-        
-        # update centroids 
-        new_centroids = update_centroids(clusters)
-        
-        if new_centroids == centroids:
-            break
-        
-        centroids = new_centroids
-    return centroids, clusters        
-
-points = [(2,10), (2,5), (8,4), (5,8), (7,5), (6,4), (1,2), (4,9)]   # point co-ordinates
-k = 3  # no of clusters
-
-centroids, clusters = k_means(points, k)
-
-for i, centroid in enumerate(centroids):
-    print('Cluster', (i + 1), 'centroid :', centroid)
-    print('Data points in Cluster', (i + 1), clusters[i])
-    print()
+plt.figure(figsize=(8, 6)) 
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis', s=50, alpha=0.7)
+plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='X', s=200, label='Centroids') 
+plt.xlabel('Feature 1') 
+plt.ylabel('Feature 2')
+plt.title('K-means Clustering')
+plt.legend()
+plt.show()
